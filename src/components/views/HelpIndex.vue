e517ca99741383a2c6e7bd727ee73ba762ecfc75
Amir Omidi <amir@aaomidi.com>
2018-02-08 16:39:13 -0800

    Improve help page

--- src/components/views/HelpIndex.vue
+++ src/components/views/HelpIndex.vue
@@ -1,44 +1,54 @@
 <template>
   <div class="container">
-    <div class="columns is-desktop">
-      <div class="column">
-        <router-link to="/help/booleans">
+    <h1 class="is-size-1 mb-20">Help</h1>
+    <div class="columns is-desktop is-8">
+      <router-link class="column" to="/help/booleans">
+        <div class="is-primary has-text-centered">
           <h2 class="is-size-3">Booleans</h2>
-        </router-link>
-      </div>
-      <div class="column">
-        <router-link to="/help/conditionals">
+        </div>
+      </router-link>
+      <router-link class="column" to="/help/conditionals">
+        <div class="is-primary has-text-centered">
           <h2 class="is-size-3">Conditionals</h2>
-        </router-link>
-      </div>
-      <div class="column">
-        <router-link to="/help/whitespace">
+        </div>
+      </router-link>
+      <router-link class="column" to="/help/whitespace">
+        <div class="is-primary has-text-centered">
           <h2 class="is-size-3">Whitespace</h2>
-        </router-link>
-      </div>
-      <div class="column">
-        <router-link to="/help/loops">
+        </div>
+      </router-link>
+      <router-link class="column" to="/help/loops">
+        <div class="is-primary has-text-centered">
           <h2 class="is-size-3">Loops</h2>
-        </router-link>
-      </div>
+        </div>
+      </router-link>
     </div>
     <div class="columns is-desktop">
-      <div class="column">
-        <router-link to="/help/RGBColor">
+      <router-link class="column" to="/help/RGBColor">
+        <div class="is-primary has-text-centered">
           <h2 class="is-size-3">RGB Color</h2>
-        </router-link>
-      </div>
-      <div class="column">
-        <router-link to="/help/pixels">
+        </div>
+      </router-link>
+      <router-link class="column" to="/help/pixels">
+        <div class="is-primary has-text-centered">
           <h2 class="is-size-3">Pixels</h2>
-        </router-link>
-      </div>
-      <div class="column">
-        <router-link to="/help/displayGrid">
+        </div>
+      </router-link>
+      <router-link class="column" to="/help/displayGrid">
+        <div class="is-primary has-text-centered">
           <h2 class="is-size-3">Display Grid</h2>
-        </router-link>
-      </div>
+        </div>
+      </router-link>
       <div class="column"></div>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+  .is-primary {
+    background-color: #00d1b2;
+    color: #FFF;
+    padding: 20px;
+    border-radius: 5px;
+  }
+</style>
